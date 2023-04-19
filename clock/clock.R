a001ed25d7672776a6c98f66b13e249e3d5ad8b7
colinleach <colin.leach@comcast.net>
2023-04-19 14:50:30 -0700

    clock passes all 52 tests

--- clock/clock.R
+++ clock/clock.R
@@ -1,3 +1,6 @@
+# Here, clock is implemented as a list, but there are other options
+# The tests ignore implementation details and only check the display string
+
 create <- function(hours, minutes) {
   total_minutes <- hours * 60 + minutes
   normalized_hours <- (total_minutes %/% 60) %% 24
@@ -5,14 +8,18 @@ create <- function(hours, minutes) {
   list(hours = normalized_hours, minutes = normalized_minutes)
 }
 
-add <- function(minutes, clock) {
-
+add <- function(clock, minutes) {
+  create(clock$hours, clock$minutes + minutes)
 }
 
-subtract <- function(minutes, clock) {
-
+subtract <- function(clock, minutes) {
+  create(clock$hours, clock$minutes - minutes)
 }
 
 display <- function(clock) {
   sprintf("%02d:%02d", clock$hours, clock$minutes)
+}
+
+equal <- function(clock1, clock2) {
+  clock1$hours == clock2$hours && clock1$minutes == clock2$minutes
 }
